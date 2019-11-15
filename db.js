ecda51ed034e9c0924096bd4468abc037b491691
JungHyoMoon <dsinhome70@gmail.com>
2019-11-15 18:42:58 +0900

    working on search / join page

---
+++ db.js
@@ -0,0 +1,41 @@
+export const videos = [
+  {
+    id: 300132,
+    title: "Video",
+    description: "This is fake video information!",
+    views: 234,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 123123,
+      name: "JH",
+      email: "jh@moon.com"
+    }
+  },
+  {
+    id: 678123,
+    title: "not a video!",
+    description: "Is this video real?",
+    views: 234,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 123123,
+      name: "JH",
+      email: "jh@moon.com"
+    }
+  },
+  {
+    id: 123456,
+    title: "You suck",
+    description: "Just same video files.. BFB",
+    views: 234,
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
+    creator: {
+      id: 123123,
+      name: "JH",
+      email: "jh@moon.com"
+    }
+  }
+];
