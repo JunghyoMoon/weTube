e942e8baef28aacdd78406c7b27dec8ba9540708
JungHyoMoon <dsinhome70@gmail.com>
2019-12-03 15:28:04 +0900

    local login user but need session

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,4 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
@@ -5,7 +6,7 @@ export const getJoin = (req, res) => {
   res.render("join", { pageTitle: "Join" });
 };
 
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
   const {
     body: { name, email, password, password2 }
   } = req;
@@ -19,9 +20,10 @@ export const postJoin = async (req, res) => {
         email
       });
       await User.register(user, password);
-      res.redirect(routes.home);
+      next();
     } catch (error) {
       console.log(error);
+      res.redirect(routes.home);
     }
     // To Do : Log User In
   }
@@ -30,9 +32,11 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) => {
   res.render("login", { pageTitle: "Login" });
 };
-export const postLogin = (req, res) => {
-  res.redirect(routes.home);
-};
+
+export const postLogin = passport.authenticate("local", {
+  failureRedirect: routes.login,
+  successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
   // To do: 로그아웃 프로세스 구현하기(홈으로 이동, 유저는 로그아웃 처리)
