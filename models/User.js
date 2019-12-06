18c14e2a230d8fae414aefa236ebe06ee0010737
JungHyoMoon <dsinhome70@gmail.com>
2019-12-06 19:00:27 +0900

    add creator to each video and small bugfix

--- models/User.js
+++ models/User.js
@@ -6,7 +6,19 @@ const UserSchema = new mongoose.Schema({
   email: String,
   avatarUrl: String,
   githubId: Number,
-  googleId: Number
+  googleId: Number,
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment"
+    }
+  ],
+  videos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Video"
+    }
+  ]
 });
 UserSchema.plugin(passportLocalMongoose, { usernameField: "email" });
 
